466683f0a0549afa57dc77047420c341a040efd1
bschwartz10 <bschwartz92@gmail.com>
2017-03-05 20:03:26 -0700

    too high task ]complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
+
+def check_guess(guess)
+  if guess.to_i > SECRET_NUMBER
+    "Too high. The secret number is #{SECRET_NUMBER}."
+  end
+end
+
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {
+    :number => SECRET_NUMBER,
+    :message => message
+  }
 end
