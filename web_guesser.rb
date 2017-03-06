893fb6ed7af3ab2cd0d549bebb21adf1b0a18fe3
bschwartz10 <bschwartz92@gmail.com>
2017-03-06 16:07:19 -0700

    I0: first sinatra server

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,27 +1,13 @@
 require 'sinatra'
-require 'sinatra/reloader'
-
-SECRET_NUMBER = rand(100)
-
-def check_guess(guess)
-  if (guess.to_i - 5) > SECRET_NUMBER
-    "Way to high!"
-  elsif (guess.to_i + 5) < SECRET_NUMBER
-    "Way to low!"
-  elsif guess.to_i > SECRET_NUMBER
-    "Too high."
-  elsif guess.to_i < SECRET_NUMBER
-    "Too low."
-  elsif guess.to_i == SECRET_NUMBER
-    "You got it right!"
-  end
-end
-
 
 get '/' do
-  guess = params["guess"]
-  message = check_guess(guess)
-  erb :index, :locals => {
-    :message => message
-  }
+  "Hello World!"
 end
+
+
+
+
+
+# get '/:name' do
+#   "Hello, my name is #{params[:name].capitalize}"
+# end
