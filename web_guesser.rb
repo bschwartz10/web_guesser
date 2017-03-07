5e65211440608c336263af549c78f0fb0a4ddcc6
bschwartz10 <bschwartz92@gmail.com>
2017-03-06 20:58:14 -0700

    add too high and too low

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,14 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
 
-get '/' do
-  erb :index, :locals => {:number => number}
+def check_guess(guess)
+  if guess.to_i > SECRET_NUMBER
+    "Too High. The number was #{SECRET_NUMBER}"
+  elsif guess.to_i < SECRET_NUMBER
+    "Too Low. The number was #{SECRET_NUMBER}"
+  end
 end
 
-
-
+get '/' do
+  guess = params['guess']
+  message = check_guess(guess)
+  erb :index, :locals => {:message =>message}
+end
 
 
 # get '/:name' do
